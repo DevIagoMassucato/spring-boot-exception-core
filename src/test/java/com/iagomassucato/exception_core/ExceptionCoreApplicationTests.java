ce6a70d78123facc48fcc313541fad48565328cd
Dev Iago Massucato <dev.iagomassucato@gmail.com>
2025-11-13 09:16:25 -0300

    chore: initialize project

---
+++ src/test/java/com/iagomassucato/exception_core/ExceptionCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iagomassucato.exception_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
